4679fe03463323b71b50c7332ae9c8ba8b26104f
aqila2310 <rama.ilham2310@gmail.com>
2021-12-02 14:55:42 +0700

    Portfolio Aqila

---
+++ app/src/main/java/com/idn/portofolio/binding_adapter/BindingAdapterExperience.kt
@@ -0,0 +1,32 @@
+package com.idn.portofolio.binding_adapter
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.idn.portofolio.data.Portofolio
+import org.w3c.dom.Text
+
+class BindingAdapterExperience {
+}
+
+@BindingAdapter("experienceTitle")
+fun TextView.setExperienceTitle(data: Portofolio?) {
+    data?.let {
+        text = data.title
+    }
+}
+
+@BindingAdapter("experienceDetail")
+fun TextView.setExperienceDetail(data: Portofolio?) {
+    data?.let {
+        text = data.detail
+    }
+}
+
+@BindingAdapter("experienceImage")
+fun ImageView.setExperienceImage(data: Portofolio) {
+    load(data.image) {
+        crossfade(1000)
+    }
+}
